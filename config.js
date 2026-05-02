97389c36e8801844d67443e6caaafe83aaffe924
Abdulrahmangit config --global user.name <abdalrhmanmahmoud12@gmail.com>
2026-05-03 01:34:43 +0300

    fix config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Check if we are running locally or on a server
-const API_BASE_URL = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
-    ? "http://127.0.0.1:5000" 
-    : "https://your-production-api.com";
